2c9da6abd13d6adef413a9d1d451a2575d95c4a0
La-wii <sentilavale@gmail.com>
2021-06-10 18:28:58 +0200

    aggiunta variabile funzione

--- es-2-stampa-parola-piu-lunga/js/script.js
+++ es-2-stampa-parola-piu-lunga/js/script.js
@@ -1,24 +1,23 @@
 // Dare la possibilità di inserire due parole. 
 
-var parolaUno = prompt('Inserisci la prima parola');
-var parolaDue = prompt('Inserisci la seconda parola');
-
+var parolaUno = prompt('Inserisci prima parola');
+var parolaDue = prompt('Inserisci seconda parola');
 
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.  
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-function wordsLength(w1, w2){
-    var wLength;
-    if (w1.length == w2.length){
-        wLength = w1 + ',' + w2 + ': queste due parole hanno la stessa lunghezza';
-    } else if (w1.length > w2.length) {
-        wLength = 'La parola più lunga è: ' + w1;
+
+function lunghezzaStringhe(x,y){
+
+    if(x.length > y.length) {
+        return x + ' è più lunga';
+    } else if (x.length < y.length) {
+        return y + ' è più lunga';
     } else {
-        wLength = 'La parola più lunga è: ' + w2;
+        return x + ',' +  + 'sono della stessa lunghezza';
     }
 
-    return wLength;
 }
 
-var resultLength = wordsLength(parolaUno, parolaDue);
-console.log(wordsLength(parolaUno, parolaDue));
+var parolaPiuLunga = lunghezzaStringhe(parolaUno,parolaDue);
+console.log(parolaPiuLunga);
